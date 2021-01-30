fd1e185aae3882f826beb8ee81ad74df3cb948d4
Jaclyn <jaclynnbarrera@gmail.com>
2021-01-30 14:00:36 -0500

    commit tree 06c62c61b94dfa4a102dd847c35f09415b6f07cc

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,32 @@
+require 'pry'
 
+class Doctor
+    attr_accessor :name
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date,patient)
+        Appointment.new(date,patient,self)
+    end
+
+    def appointments
+        #iterates through all appointments and finds those belonging to this doctor
+        Appointment.all.select {|appt|appt.doctor == self}
+    end
+
+    def patients
+        appointments.collect do |appt|
+            appt.patient
+        end
+    end
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,20 @@
+#joiner
+require 'pry'
 
+class Song
+
+    @@all = []
+
+    attr_accessor :artist, :genre, :name
+    
+    def initialize(name,artist,genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
